5dba82e31a923a4488663ae1c3af23c1ff675886
Andras Csanyi <andras.csanyi@pm.me>
2022-07-29 13:05:11 +0200

    impl

---
+++ src/main/java/com/encyclopediagalactica/sourceformats/controllers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.encyclopediagalactica.sourceformats.controllers;
+
+import javax.validation.ConstraintViolationException;
+import com.encyclopediagalactica.sourceformats.errors.ErrorMessages;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+  
+  @ExceptionHandler(value = {ConstraintViolationException.class, InvalidDataAccessApiUsageException.class})
+  protected ResponseEntity<Object> handleConflict(RuntimeException runtimeException, WebRequest webRequest
+  ) {
+    return handleExceptionInternal(
+        runtimeException,
+        ErrorMessages.VALIDATION_ERROR,
+        new HttpHeaders(),
+        HttpStatus.BAD_REQUEST,
+        webRequest);
+  }
+  
+}
