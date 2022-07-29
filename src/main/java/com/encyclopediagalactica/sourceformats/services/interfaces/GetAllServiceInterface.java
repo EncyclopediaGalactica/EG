0f68d3ca2947adce50dfad94ea4e05c4d9ba7f15
Andras Csanyi <andras.csanyi@pm.me>
2022-07-29 23:07:55 +0200

    tests

--- src/main/java/com/encyclopediagalactica/sourceformats/services/interfaces/GetAllServiceInterface.java
+++ src/main/java/com/encyclopediagalactica/sourceformats/services/interfaces/GetAllServiceInterface.java
@@ -4,6 +4,15 @@ package com.encyclopediagalactica.sourceformats.services.interfaces;
 import java.util.List;
 import com.encyclopediagalactica.sourceformats.dto.SourceFormatDto;
 
+/**
+ * GetAllServiceInterface.
+ */
 public interface GetAllServiceInterface {
+
+  /**
+   * It returns list of SourceFormatDtos representing entities in the database
+   * 
+   * @return list of SourceFormatDtos
+   */
   List<SourceFormatDto> getAll();
 }
